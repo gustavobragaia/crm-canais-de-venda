1098450b675e64c391a53824d70e759aa0cdca3c
Gustavo <gustavobragaia12@gmail.com>
2026-03-08 16:24:24 -0300

    2 to 1

--- lib/db.ts
+++ lib/db.ts
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 function createClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL!, max: 2 })
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL!, max: 1 })
   return new PrismaClient({ adapter })
 }
 
